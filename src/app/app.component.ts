56e841c37993706408106597617dbf0aca803e97
Jaime Tellie <jaimetellie@Jaimes-Air.home>
2024-03-03 16:16:13 +0100

    Added inital card to home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Home', url: '/folder/home', icon: 'mail' },
+    { title: 'Contact', url: '/folder/contact', icon: 'paper-plane' },
+    { title: 'About Me', url: '/folder/about', icon: 'heart' }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Deep Learning', 'Generative AI', 'Large Language Models', 'AWS', 'SageMaker'];
   constructor() {}
 }
